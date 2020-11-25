f85aa53498340a28097073c0b037473328df050b
Neisser Villa <neisser.villa@pluriza.com>
2020-11-25 14:35:51 -0500

    initial commit

---
+++ calculator.go
@@ -0,0 +1,64 @@
+package myCalculator
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type calc struct{}
+
+func (calc) parseInteger(operated string) (int, error) {
+	result, err := strconv.Atoi(operated)
+	return result, err
+}
+
+func (c calc) operate(input string, operator string) (int, error) {
+	cleanInput := strings.Split(input, operator)
+	first, err := c.parseInteger(cleanInput[0])
+	if err != nil {
+		return 0, err
+	}
+	second, err := c.parseInteger(cleanInput[1])
+	if err != nil {
+		return 0, err
+	}
+	switch operator {
+	case "+":
+		return first + second, nil
+	case "-":
+		return first - second, nil
+	case "*":
+		return first * second, nil
+	case "/":
+		return first / second, nil
+	default:
+		fmt.Println(operator, "Operator is not supported!")
+		return 0, nil
+	}
+}
+
+func readInput() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func processResult(input string, operator string) {
+	c := calc{}
+	value, err := c.operate(input, operator)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(value)
+	}
+}
+
+// Main calculator function
+// func main() {
+// 	input := readInput()
+// 	operator := readInput()
+// 	processResult(input, operator)
+// }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Neisser/my-calculator
+
+go 1.15
